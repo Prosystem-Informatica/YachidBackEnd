fd8478f83d1da7d6a4c57c6fd6987bcb93378c4f
lucas henrique <lucas.dpl@hotmail.com>
2025-12-08 00:56:13 -0300

    fix:fix errors in product

---
+++ src/modules/product/updateProduct/UpdateProductUseCase.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from "tsyringe";
+import { ProductRepository } from "../repositories/ProductRepository";
+
+@injectable()
+export class UpdateProductUseCase {
+  constructor(
+    @inject("ProductRepository")
+    private productRepository: ProductRepository
+  ) {}
+
+  async execute(id: number, data: Partial<any>) {
+    const existingProduct = await this.productRepository.findById(id);
+    if (!existingProduct) {
+      throw new Error("Produto não encontrado");
+    }
+
+    const updatedProduct = await this.productRepository.update(id, data);
+
+    return updatedProduct;
+  }
+}
